4659bbcf69079123d9a28459d7aafb20f0348157
Ben Walton <ben.walton9@gmail.com>
2019-06-29 20:25:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === shout(string)) {
-    return "YES INDEED"
+    return "YES INDEED!"
   } else if (string === whisper(string)){
     return "I can\'t hear you!"
   } else if (string === "I love you, Grandma.") {
